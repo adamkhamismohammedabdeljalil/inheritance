8acf4ee9787b13eb49fe56787644926cf73741e6
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-06-26 13:00:58 +0200

    feat:add base class Animal and derive classes Dog and Snake

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,43 @@
+﻿using Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Inheritance
+{
+    //base class Animal
+    internal class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public virtual void Speak()
+        {
+            Console.WriteLine($"The {Name} is speaking");
+        }
+    }
+
+}
+//derived Dog class
+internal class Dog : Animal
+{
+
+    public override void Speak()
+    {
+        Console.WriteLine($"{Name} is barking");
+
+    }
+
+
+}
+
+//derived Snake class
+internal class Snake : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine($" {Name} is rattling");
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //instantiate Dog class
+            Dog dog = new Dog();
+            dog.Name = "Jargass";
+            dog.Speak();
+
+            //instantiate Snake class
+            Snake snake = new Snake();
+            snake.Name = "Randy";
+            snake.Speak();
+        }
+    }
+}
